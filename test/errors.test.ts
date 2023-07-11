161a97f0cb90e799cc51047fbdda1bf0a4d8d342
stevegoossens <steve.goossens@bbc.co.uk>
2023-07-12 00:02:13 +0100

    add ApiError tests

---
+++ test/errors.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from '@jest/globals';
+import { ApiError } from '../src/errors';
+
+describe('ApiError', () => {
+  it('should instantiate without parameters', () => {
+    // given
+
+    // when
+    const apiError = new ApiError()
+
+    // then
+    expect(apiError).toHaveProperty('message', 'API Error')
+  })
+
+  it('should instantiate with specific message', () => {
+    // given
+    const message = 'specific'
+
+    // when
+    const apiError = new ApiError(message)
+
+    // then
+    expect(apiError).toHaveProperty('message', message)
+  })
+
+  it('should instantiate with optional cause', () => {
+    // given
+    const message = 'specific'
+    const cause = new Error('plop')
+
+    // when
+    const apiError = new ApiError(message, { cause })
+
+    // then
+    expect(apiError).toHaveProperty('message', message)
+    expect(apiError).toHaveProperty('cause', cause)
+  })
+})
