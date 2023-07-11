0d4e1f80db54368bcd25bb85c22952eb75fd177a
stevegoossens <steve.goossens@bbc.co.uk>
2023-07-11 01:38:39 +0100

    complete implementation and add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,16 @@ export class PostcodesIO {
     this._terminatedPostcodesApi = new TerminatedPostcodesApi(configuration)
   }
 
+  /**
+   * Accepts an array of postcodes. Returns a list of matching postcodes and
+   * respective available data. Accepts up to 100 postcodes.
+   * 
+   * @summary Bulk Postcode Lookup
+   * @param {string[]} postcodes List of postcodes
+   * @param {Pick<PostcodesApi.BulkPostcodeLookupOrBulkReverseGeocodingParameters, 'filter'>} [options] Optional options
+   * @param {string} [options.filter] A comma separated whitelist of attributes to be returned in the result object(s), e.g. `postcode,longitude,latitude`.
+   * @returns {Promise<Api.PostcodeData[] | undefined>} List of PostcodeData, or undefined
+   */
   async bulkPostcodeLookup(
     postcodes: string[],
     options?: Pick<PostcodesApi.BulkPostcodeLookupOrBulkReverseGeocodingParameters, 'filter'>
@@ -43,6 +53,17 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Bulk translates geolocations into Postcodes. Accepts up to 100 geolocations.
+   * 
+   * @summary Bulk Reverse Geocoding
+   * @param {Geolocation[]} geolocations List of geolocations
+   * @param {Omit<PostcodesApi.BulkPostcodeLookupOrBulkReverseGeocodingParameters, 'filter'>} [options] Optional options
+   * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+   * @param {number} [options.radius] Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+   * @param {boolean} [options.widesearch] Applies a global widesearch parameter to all geolocation lookup objects. Defaults to `false`.
+   * @returns {Promise<Api.PostcodeData[] | undefined>} List of PostcodeData, or undefined
+   */
   async bulkReverseGeocoding(
     geolocations: Api.Geolocation[],
     options?: Omit<PostcodesApi.BulkPostcodeLookupOrBulkReverseGeocodingParameters, 'filter'>
@@ -60,9 +81,19 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Returns nearest outcodes for a given outcode.
+   * 
+   * @summary Nearest Outcode
+   * @param {string} outcode The outward code is the part of the postcode before the single space in the middle.
+   * @param {Omit<OutcodesApi.NearestOutcodeParameters, 'outcode'>} [options] Optional options
+   * @param {number} [options.limit] Limits number of outcodes matches to return. Defaults to 10. Needs to be less than 100.
+   * @param {number} [options.radius] Limits number of outcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+   * @returns {Promise<Api.OutcodeData[] | undefined>} List of OutcodeData, or undefined
+   */
   async nearestOutcode(
-      outcode: string,
-      options?: Omit<OutcodesApi.NearestOutcodeParameters, 'outcode'>
+    outcode: string,
+    options?: Omit<OutcodesApi.NearestOutcodeParameters, 'outcode'>
   ): Promise<Api.OutcodeData[] | undefined> {
     const params: OutcodesApi.NearestOutcodeParameters = {
       ...{ outcode },
@@ -74,6 +105,41 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Returns nearest postcodes for a given postcode.
+   * 
+   * @summary Nearest Postcode
+   * @param {string} postcode All current (‘live’) postcodes within the United Kingdom, the Channel Islands and the Isle of Man.
+   * @param {Omit<PostcodesApi.NearestPostcodeParameters, 'postcode'>} [options] Optional options
+   * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+   * @param {number} [options.radius] Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+   * @returns {Promise<Api.PostcodeData[] | undefined>} List of PostcodeData, or undefined
+   */
+  async nearestPostcode(
+    postcode: string,
+    options?: Omit<PostcodesApi.NearestPostcodeParameters, 'postcode'>
+  ): Promise<Api.PostcodeDataReverseGeocoding[] | undefined> {
+    const params: PostcodesApi.NearestPostcodeParameters = {
+      ...{ postcode },
+      ...options,
+    }
+    const response = await this._postcodesApi.nearestPostcode(params)
+    if (response.status === 200 && response.body.status === 200 && response.body.result) {
+      return response.body.result
+    }
+  }
+
+  /**
+   * Returns nearest outcodes for a given longitude and latitude.
+   * 
+   * @summary Outcode Reverse Geocoding
+   * @param {number} lon Longitude
+   * @param {number} lat Latitude
+   * @param {Omit<OutcodesApi.OutcodeReverseGeocodingParameters, 'lon' | 'lat'>} [options] Optional options
+	 * @param {number} [options.limit] Limits number of outcodes matches to return. Defaults to 10. Needs to be less than 100.
+	 * @param {number} [options.radius] Limits number of outcodes matches to return. Defaults to 5000m. Needs to be less than 25,000m.
+   * @returns {Promise<Api.OutcodeData[] | undefined>} List of OutcodeData, or undefined
+   */
   async outcodeReverseGeocoding(
     lon: number,
     lat: number,
@@ -89,6 +155,15 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Submit a place query and receive a complete list of places matches and associated data.
+   * 
+	 * @summary Place Query
+   * @param {string} query Postcode search (prefix match)
+   * @param {Omit<PlacesApi.PlaceQueryParameters, 'q' | 'query'>} [options]
+	 * @param {number} [options.limit] Limits number of places matches to return. Defaults to 10. Needs to be less than 100.
+   * @returns {Promise<Api.PlacesData[] | undefined>} List of PlacesData, or undefined
+   */
   async placeQuery(
     query: string,
     options?: Omit<PlacesApi.PlaceQueryParameters, 'q' | 'query'>
@@ -103,20 +178,93 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Find a place by OSGB code (e.g. "osgb4000000074564391").
+   * Returns all available data if found.
+   * 
+	 * @summary Place Lookup
+   * @param {string} code A unique identifier that enables records to be identified easily. The identifier will be persistent for all LocalTypes except Section of Named Road and Section of Numbered Road.
+   * @returns {Promise<Api.PlacesData | undefined>} PlacesData or undefined
+   */
   async placeLookup(code: string): Promise<Api.PlacesData | undefined> {
-    const response = await this._placesApi.placeLookup(code)
-    if (response.status === 200 && response.body.status === 200 && response.body.result) {
-      return response.body.result
+    try {      
+      const response = await this._placesApi.placeLookup(code)
+      if (response.status === 200 && response.body.status === 200 && response.body.result) {
+        return response.body.result
+      }
+    } catch (error) {
+      return undefined
+    }
+    return undefined
+  }
+
+  /**
+   * Convenience method to return a list of matching postcodes.
+   * 
+	 * @summary Postcode Autocomplete
+   * @param {string} postcode Postcode search (prefix match)
+   * @param {Omit<PostcodesApi.PostcodeAutocompleteParameters, 'postcode'>} [options] Optional options
+   * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+   * @returns {Promise<string[] | undefined>} List of postcodes, or undefined
+   */
+  async postcodeAutocomplete(
+    postcode: string,
+    options?: Omit<PostcodesApi.PostcodeAutocompleteParameters, 'postcode'>
+  ): Promise<string[] | undefined> {
+    try {
+      const params: PostcodesApi.PostcodeAutocompleteParameters = {
+        ...{ postcode },
+        ...options,
+      }
+      const response = await this._postcodesApi.postcodeAutocomplete(params)
+      if (response.status === 200 && response.body.status === 200 && response.body.result) {
+        return response.body.result
+      }
+    } catch (error) {
+      return undefined
     }
   }
 
-  async postcodeLookup(postcode: string) {
-    const response = await this._postcodesApi.postcodeLookup(postcode)
-    if (response.status === 200 && response.body.status === 200 && response.body.result) {
-      return response.body.result
+  /**
+   * This uniquely identifies a postcode.
+   * 
+	 * Returns a single postcode entity for a given postcode (case, space insensitive).
+	 * 
+	 * @summary Postcode Lookup
+   * @param {string} postcode All current (‘live’) postcodes within the United Kingdom, the Channel Islands and the Isle of Man.
+   * @returns {Api.PostcodeData | undefined} PostcodeData or undefined
+   */
+  async postcodeLookup(postcode: string): Promise<Api.PostcodeData | undefined> {
+    try {      
+      const response = await this._postcodesApi.postcodeLookup(postcode)
+      if (response.status === 200 && response.body.status === 200 && response.body.result) {
+        return response.body.result
+      }
+    } catch (error) {
+      return undefined
     }
   }
 
+  /**
+	 * Submit a postcode query and receive a complete list of postcode matches and
+   * all associated postcode data.
+   * 
+	 * This is essentially a postcode search which prefix matches and returns
+   * postcodes in sorted order (case insensitive).
+   * 
+	 * This is space sensitive, i.e. it detects for spaces between outward and
+   * inward parts of the postcode.
+   * 
+	 * The result set can either be empty or populated with up to 100 postcode entities.
+   * 
+	 * @summary Postcode Query
+   * @param {string} query Postcode search (prefix match)
+   * @param {Omit<PostcodesApi.ReverseGeocodingOrPostcodeQueryParameters, 'lon' | 'lat' | 'q' | 'query'>} [options] Optional options 
+	 * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+	 * @param {number} [options.radius] Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+	 * @param {boolean} [options.widesearch] Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we've created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+   * @returns {Promise<Api.PostcodeDataReverseGeocoding[] | undefined>} List of PostcodeDataReverseGeocoding, or undefined
+   */
   async postcodeQuery(
     query: string,
     options?: Omit<PostcodesApi.ReverseGeocodingOrPostcodeQueryParameters, 'lon' | 'lat' | 'q' | 'query'>
@@ -131,6 +279,31 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Convenience method to validate a postcode.
+   * 
+	 * @summary Postcode Validation
+   * @param {string} postcode All current (‘live’) postcodes within the United Kingdom, the Channel Islands and the Isle of Man.
+   * @returns {Promise<boolean>} true or false (meaning valid or invalid respectively)
+   */
+  async postcodeValidation(postcode: string): Promise<boolean> {
+    try {      
+      const response = await this._postcodesApi.postcodeValidation(postcode)
+      if (response.status === 200 && response.body.status === 200 && response.body.result) {
+        return response.body.result
+      }
+    } catch (error) {
+      return false
+    }
+    return false
+  }
+
+  /**
+   * Returns a random place and all associated data
+   * 
+	 * @summary Random Place
+   * @returns {Promise<Api.PlacesData | undefined>} PlacesData or undefined
+   */
   async randomPlace(): Promise<Api.PlacesData | undefined> {
     const response = await this._randomApi.randomPlace()
     if (response.status === 200 && response.body.status === 200 && response.body.result) {
@@ -138,6 +311,13 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Returns a random postcode and all available data for that postcode.
+   * 
+	 * @summary Random Postcode
+   * @param {string} [outcode] Filters random postcodes by outcode.
+   * @returns {Promise<Api.PostcodeData | undefined>} PostcodeData or undefined
+   */
   async randomPostcode(outcode?: string): Promise<Api.PostcodeData | undefined> {
     const response = await this._randomApi.randomPostcode(outcode)
     if (response.status === 200 && response.body.status === 200 && response.body.result) {
@@ -145,6 +325,18 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Returns nearest postcodes for a given longitude and latitude.
+   * 
+	 * @summary Reverse Geocoding
+   * @param {number} lon Longitude
+   * @param {number} lat Latitude
+   * @param {Omit<PostcodesApi.ReverseGeocodingOrPostcodeQueryParameters, 'lon' | 'lat' | 'q' | 'query'>} [options] Optional options 
+	 * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+	 * @param {number} [options.radius] Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+	 * @param {boolean} [options.widesearch] Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we&#39;ve created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+   * @returns {Promise<Api.PostcodeData[] | undefined>} List of PostcodeData, or undefined
+   */
   async reverseGeocoding(
     lon: number,
     lat: number,
@@ -160,6 +352,41 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Returns nearest postcodes for a given longitude and latitude.
+   * 
+	 * @summary Reverse Geocoding (Legacy)
+   * @param {number} longitude Longitude
+   * @param {number} latitude Latitude
+   * @param {Omit<PostcodesApi.ReverseGeocodingLegacyParameters, 'longitude' | 'latitude'>} [options] Optional options
+	 * @param {number} [options.limit] Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+	 * @param {number} [options.radius] Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+	 * @param {boolean} [options.widesearch] Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we&#39;ve created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+   * @returns {Promise<Api.PostcodeDataReverseGeocoding[] | undefined>} List of PostcodeDataReverseGeocoding, or undefined
+   */
+  async reverseGeocodingLegacy(
+    longitude: number,
+    latitude: number,
+    options?: Omit<PostcodesApi.ReverseGeocodingLegacyParameters, 'longitude' | 'latitude'>
+  ): Promise<Api.PostcodeDataReverseGeocoding[] | undefined> {
+    const params: PostcodesApi.ReverseGeocodingLegacyParameters = {
+      ...{ longitude, latitude },
+      ...options,
+    }
+    const response = await this._postcodesApi.reverseGeocodingLegacy(params)
+    if (response.status === 200 && response.body.status === 200 && response.body.result) {
+      return response.body.result
+    }
+  }
+
+  /**
+   * Lookup a Scottish postcode. Returns SPD data associated with postcode.
+   * At the moment this is just Scottish Parliamentary Constituency.
+   * 
+	 * @summary Scottish Postcode Lookup
+   * @param {string} postcode Scottish postcode
+   * @returns {Promise<Api.ScottishPostcodeData | undefined>} ScottishPostcodeData or undefined
+   */
   async scottishPostcodeLookup(postcode: string): Promise<Api.ScottishPostcodeData | undefined> {
     const response = await this._scotlandApi.scottishPostcodeLookup(postcode)
     if (response.status === 200 && response.body.status === 200 && response.body.result) {
@@ -167,6 +394,14 @@ export class PostcodesIO {
     }
   }
 
+  /**
+   * Lookup a terminated postcode. Returns the postcode, year and month of
+   * termination.
+   * 
+	 * @summary Terminated Postcode Lookup
+   * @param {string} postcode Teriminated postcode
+   * @returns {Promise<Api.TerminatedPostcodeData | undefined>} TerminatedPostcodeData or undefined
+   */
   async terminatedPostcodeLookup(postcode: string): Promise<Api.TerminatedPostcodeData | undefined> {
     const response = await this._terminatedPostcodesApi.terminatedPostcodeLookup(postcode)
     if (response.status === 200 && response.body.status === 200 && response.body.result) {
