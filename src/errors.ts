be48db84ff0b15b45a1bfa1509d3545161970ffa
stevegoossens <steve.goossens@bbc.co.uk>
2023-07-11 23:48:49 +0100

    add custom error

---
+++ src/errors.ts
@@ -0,0 +1,20 @@
+/**
+ * A consistent error with optional cause
+ * 
+ * @export
+ * @class ApiError
+ * @extends {Error}
+ */
+export class ApiError extends Error {
+  /**
+   * @constructor
+   * @param {string} message Error message (default = "API Error")
+   * @param {ErrorOptions} [options] Optional options
+   * @param {unknown} [options.cause] Error cause (e.g. another Error)
+   */
+	constructor(message: string = 'API Error', options?: ErrorOptions) {
+		super(message, options);
+		Object.setPrototypeOf(this, ApiError.prototype);
+		this.name = "ApiError";
+	}
+}
